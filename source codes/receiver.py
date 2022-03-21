5a1d45eea8c6fe3d5b5836cc97eaf4a02b293362
Bhaskar Panja <bhaskarpanja93@gmail.com>
2022-03-21 09:30:56 +0530

    FileShare 2.0 Completely Revamped Directory Cloner
    New:
    >> Buffer size changed from 5 KB to 10 KB, faster speed, less CPU usage, at the expense of bit more RAM.
    >> Dynamic Port used to connect the 2 devices(Receiver hosts [65535:1:-500] ports for Sender to connect).
    >> Max concurrent exchanges reduced to 50, this will improve network stability in situations like weak Wi-Fi devices. Also fixed the bug where the program used to bypass the MAX_SIMULTANEOUS_FILES limit, fixed by adding new parameter FILES_QUEUED_UP(number of files queued up to be sent before authentication is complete).
    >> Reduced the secret key length from 5 to 4(Must have reduced a lot of hassle memorising the key). Also, secret key is now generated and can be entered only after the 2 devices finish initial handshake.
    >> Completely revamped file data, Sends the partial data length in first 15bytes, then the actual data. Reduces wait time in high latency networks(earlier both devices had to wait 2 times each for each chunk of data, now reduced to 1). If complete file is finished sending, a blank byte is sent(interpreted as False-bool by receiver).
    >> Sender no longer sends full file size.
    >> If connection fails between the 2 devices, same file is resent(new connection is made). Hope it works.
    >> Update screen function fully revamped. New max delay between the start of file sending and screen update in 0.1sec. New max delay between end of file sharing and screen update is 0.5sec. Update screen now shows Number of Currently cloning files, Number of files finished cloning, Total Data sent, Total Data received and Time elapsed.
    >> Data sent and Data received now counts all data sent and received and not just file sizes(Data Exchanged for basic communication is also counted).
    >> Receiver paused file receive authentication if wrong secret key is entered till the correct key is provided.
    
    Incomplete:
    >> Global file cloning using Ngrok.
    >> Duration calculator in Hours, Minutes, Seconds instead of just seconds.
    >> If connection drops, files are resent, which causes total files exchanged to be incorrect.

--- source codes/receiver.py
+++ source codes/receiver.py
@@ -4,32 +4,38 @@ from platform import system
 import socket
 from threading import Thread
 from time import sleep, time
+
 secret_key = b''
-FILES_CURRENTLY_RECEIVING=0
-FILES_RECEIVED = 0
-SIZE_RECEIVED = 0
-SERVER_MIGRATION_PORT = 60000
-BUFFER_SIZE = 1024*5
+FILES_CURRENTLY_RECEIVING = FILES_RECEIVED = SIZE_RECEIVED = SIZE_SENT = 0
+secret_key_accepted = False
+reachable_port = None
+BUFFER_SIZE = 1024*10
 FINISHED = False
+USER_DEFINED_LIMIT = 50
 
 
-
-"""if system() == 'Windows':
-    system_caller(f'netsh advfirewall firewall add rule name= "_BhaskarFileTransfer" dir=in action=allow protocol=TCP localport={SERVER_MIGRATION_PORT}')"""
-
-for _ in range(10000):
+MAX_SIMULTANEOUS_FILES = 0
+i = 0
+while True:
+    i+=1
     try:
-        globals()[f"_{_}"] = open('x','ab')
+        globals()[f"_{i}"] = open('x','ab')
     except:
-        MAX_SIMULTANEOUS_FILES=int(_*0.5)
-        for _ in range(_):
-            try:
-                globals()[f"_{_}"].close()
-                del globals()[f"_{_}"]
-            except:
-                pass
-        break
+        MAX_SIMULTANEOUS_FILES = True
+    finally:
+        if MAX_SIMULTANEOUS_FILES or i == USER_DEFINED_LIMIT * 2:
+            MAX_SIMULTANEOUS_FILES = int(i // 2)
+            for _ in range(i+1):
+                try:
+                    globals()[f"_{_}"].close()
+                    del globals()[f"_{_}"]
+                except:
+                    pass
+            break
 remove('x')
+
+
+
 
 def size_calculator(file_size_raw: int):
     file_size=''
@@ -50,36 +56,88 @@ def size_calculator(file_size_raw: int):
     return file_size
 
 
+def duration_calculator(start: float, end: float):
+    final_duration = ''
+    duration_tree = []
+    units = ['sec', 'minute', 'hour', 'day']
+
+
+
+def host_ports_for_reachability(port):
+    global reachable_port
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('0.0.0.0', port))
+        sock.listen()
+        while True:
+            while reachable_port:
+                sleep(1)
+            connection, address = sock.accept()
+            while reachable_port:
+                sleep(1)
+            reachable_port = True
+            current_port = True
+            if __receive_from_connection(connection) == b'reach':
+                while reachable_port and not current_port:
+                    sleep(1)
+                __send_to_connection(connection, b'pass')
+                reachable_port = port
+                sock.close()
+            else:
+                reachable_port = None
+    except:
+        pass
+
 
 def __initial_authenticator():
-    global secret_key
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('0.0.0.0', SERVER_MIGRATION_PORT))
-    sock.listen()
+    global secret_key, secret_key_accepted
     local_ip = socket.gethostbyname_ex(socket.gethostname())[-1]
     if local_ip[0:3] == '127':
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('10.255.255.255', 1))
         local_ip = s.getsockname()[0]
     print(f'waiting at...  {local_ip}')
+    for port in range(65534, 1, -500):
+        Thread(target=host_ports_for_reachability, args=(port,)).start()
+    while type(reachable_port) != int:
+        sleep(1)
+    print(f"Connected")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('0.0.0.0', reachable_port))
+    sock.listen()
     while True:
         if secret_key:
             if FILES_CURRENTLY_RECEIVING < MAX_SIMULTANEOUS_FILES:
                 connection, address = sock.accept()
-                if __receive_from_connection(connection) == secret_key:
-                    __send_to_connection(connection, b'0')
-                    Thread(target=__file_receive, args=(connection,)).start()
+                if not secret_key_accepted:
+                    authenticate_with_sender(connection)
                 else:
-                    __send_to_connection(connection, b'-1')
-                    connection.close()
-                    secret_key = input("Wrong Secret Key entered try again: ").encode()
+                    Thread(target=authenticate_with_sender, args=(connection,)).start()
         else:
             secret_key = input("Enter Secret Key as shown in sender: ").encode()
 
 
+def authenticate_with_sender(connection):
+    global secret_key, secret_key_accepted
+    try:
+        __send_to_connection(connection, secret_key)
+        if __receive_from_connection(connection) == b'0':
+            secret_key_accepted = True
+            Thread(target=__file_receive, args=(connection,)).start()
+        else:
+            __send_to_connection(connection, b'-1')
+            connection.close()
+            secret_key = b''
+            secret_key = input("Wrong Secret Key entered try again: ").encode()
+    except:
+        try:
+            connection.close()
+        except:
+            pass
+
 
 def __file_receive(connection):
-    global FILES_CURRENTLY_RECEIVING, FILES_RECEIVED, SIZE_RECEIVED
+    global FILES_CURRENTLY_RECEIVING, FILES_RECEIVED
     try:
         FILES_CURRENTLY_RECEIVING += 1
         path = __receive_from_connection(connection).decode()
@@ -95,10 +153,9 @@ def __file_receive(connection):
             open(path, 'wb').close()
         with open(path, 'ab') as file:
             while True:
-                partial_file_data = __receive_from_connection(connection)
-                if partial_file_data != b'{-0}':
-                    SIZE_RECEIVED += len(partial_file_data)
-                    file.write(partial_file_data)
+                data = __receive_file_data(connection)
+                if data:
+                    file.write(data)
                 else:
                     file.close()
                     break
@@ -108,55 +165,83 @@ def __file_receive(connection):
         FILES_RECEIVED += 1
         connection.close()
     except:
-        FILES_CURRENTLY_RECEIVING -= 1
+        try:
+            connection.close()
+        except:
+            pass
+
+
+
+def __receive_file_data(connection):
+    global SIZE_RECEIVED
+    length = b''
+    while len(length) != 15:
+        length += connection.recv(15- len(length))
+    length = int(length)
+    SIZE_RECEIVED += length
+    if length:
+        data = b''
+        while len(data) != length:
+            data += connection.recv(length - len(data))
+        connection.send(b'-')
+        return data
+    else:
+        connection.send(b'-')
+        return False
 
 
 def __send_to_connection(connection, data_bytes: bytes):
-    connection.send(str(len(data_bytes)).encode())
+    global SIZE_SENT
+    data_byte_length = len(data_bytes)
+    connection.send(str(data_byte_length).encode())
     if connection.recv(1) == b'-':
         connection.send(data_bytes)
     if connection.recv(1) == b'-':
-        return
+        SIZE_SENT += data_byte_length
 
 
 def __receive_from_connection(connection):
+    global SIZE_RECEIVED
     length = int(connection.recv(BUFFER_SIZE))
     connection.send(b'-')
     data_bytes = b''
     while len(data_bytes) != length:
         data_bytes += connection.recv(BUFFER_SIZE)
     connection.send(b'-')
+    SIZE_RECEIVED += length
     return data_bytes
 
 
-def receiving_complete():
-    global FINISHED
-    while FILES_CURRENTLY_RECEIVING == 0:
-        sleep(1)
-    s_time = time()
-    Thread(target=update_screen).start()
-    while FILES_CURRENTLY_RECEIVING != 0:
-        sleep(1)
-    f_time = time()
-    FINISHED = True
-    """if system() == 'Windows':
-        system_caller(f'netsh advfirewall firewall delete rule name="_BhaskarFileTransfer"')"""
-    print('Receiving Complete', flush=True)
-    print(f"Total Received: {FILES_RECEIVED} Files ({size_calculator(SIZE_RECEIVED)})", flush=True)
-    print(f"Time Taken: {int(f_time-s_time)}seconds with Buffer_size: {BUFFER_SIZE}", flush=True)
-    input()
+
 
 def update_screen():
     global FILES_CURRENTLY_RECEIVING, FINISHED
+    while FILES_CURRENTLY_RECEIVING == 0:
+        sleep(0.1)
+    s_time = time()
     while not FINISHED:
-        if system() == 'Windows':
-            call('cls', shell=True)
+        if FILES_CURRENTLY_RECEIVING != 0:
+            sleep(0.5)
         else:
-            call('clear', shell=True)
-        print(f"Currently receiving: {FILES_CURRENTLY_RECEIVING} Files", flush=True)
-        print(f"Total Received: {FILES_RECEIVED} Files ({size_calculator(SIZE_RECEIVED)})", flush=True)
-        sleep(1/2)
+            FINISHED = True
+        if not FINISHED:
+            if system() == 'Windows':
+                call('cls', shell=True)
+            else:
+                call('clear', shell=True)
+            print('-' * 17, flush=True)
+            print('RECEIVING REPORT', flush=True)
+            print('-' * 17, flush=True)
+            print(f"Currently: {FILES_CURRENTLY_RECEIVING} Files", flush=True)
+            print(f"Completed: {FILES_RECEIVED} Files", flush=True)
+            print(f"Sent Data: {size_calculator(SIZE_SENT)}", flush=True)
+            print(f"Received Data: {size_calculator(SIZE_RECEIVED)}", flush=True)
+            print(f"Time Elapsed: {int(time()-s_time)} seconds", flush=True)
+    print('-' * 9, flush=True)
+    print('FINISHED')
+    print('-' * 9, flush=True)
+    input()
 
 Thread(target=__initial_authenticator).start()
-Thread(target=receiving_complete).start()
+Thread(target=update_screen).start()
 
--- source codes/sender.py
+++ source codes/sender.py
@@ -1,4 +1,4 @@
-from os import walk, getcwd, path, remove, stat
+from os import walk, getcwd, path, remove
 from subprocess import call
 from platform import system
 from random import randrange
@@ -7,33 +7,37 @@ import socket
 from time import sleep, time
 
 
-
-FILES_CURRENTLY_SENDING = 0
-FILES_SENT = 0
-SIZE_SENT = 0
-SERVER_MIGRATION_PORT = 60000
-BUFFER_SIZE = 1024*5
-FINISHED = False
-
 secret_key = b''
-for _ in range(5):
-    secret_key += chr(randrange(97,123)).encode()
-print(f"Enter this Secret Key on Receiving Side: {secret_key.decode()}")
+FILES_CURRENTLY_SENDING = FILES_SENT = SIZE_SENT = SIZE_RECEIVED = FILES_QUEUED_UP = 0
+receiver_ip = ''
+reachable_port = None
+BUFFER_SIZE = 1024*10
+FINISHED = False
+USER_DEFINED_LIMIT = 50
 
 
-for _ in range(10000):
+
+MAX_SIMULTANEOUS_FILES = 0
+i = 0
+while True:
+    i+=1
     try:
-        globals()[f"_{_}"] = open('x','ab')
+        globals()[f"_{i}"] = open('x','ab')
     except:
-        MAX_SIMULTANEOUS_FILES=int(_*0.4)
-        for _ in range(_):
-            try:
-                globals()[f"_{_}"].close()
-                del globals()[f"_{_}"]
-            except:
-                pass
-        break
+        MAX_SIMULTANEOUS_FILES = True
+    finally:
+        if MAX_SIMULTANEOUS_FILES or i == USER_DEFINED_LIMIT * 2:
+            MAX_SIMULTANEOUS_FILES = int(i // 2)
+            for _ in range(i+1):
+                try:
+                    globals()[f"_{_}"].close()
+                    del globals()[f"_{_}"]
+                except:
+                    pass
+            break
 remove('x')
+
+
 
 def size_calculator(file_size_raw: int):
     file_size=''
@@ -53,96 +57,183 @@ def size_calculator(file_size_raw: int):
     return file_size
 
 
+def find_reachable_port(ip: str, port: int, connection_type: str):
+    global reachable_port
+    if connection_type == 'tcp':
+        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    else:
+        connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    connection.settimeout(5)
+    try:
+        connection.connect((ip, port))
+        __send_to_connection(connection, b'reach')
+        if __receive_from_connection(connection) == b'pass':
+            if not reachable_port:
+                reachable_port = port
+    except:
+        pass
 
-def __initial_authenticate(new_server_ip):
-    global FILES_CURRENTLY_SENDING
+
+def __force_connect_to_receiver(ip, port, connection_type):
+    if connection_type == 'tcp':
+        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    else:
+        connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while True:
+        try:
+            connection.connect((ip, port))
+            break
+        except:
+            pass
+    return connection
+
+
+def address_translator(receiver_address: str):
+    global receiver_ip, reachable_port, secret_key
+    if ':' in receiver_address: #global address not implemented
+        receiver_ip, reachable_port = receiver_address.split(':')
+    else:
+        receiver_ip = receiver_address
+        for port in range(65534,1,-500):
+            if reachable_port:
+                break
+            else:
+                Thread(target=find_reachable_port, args=(receiver_ip, port, 'tcp',)).start()
+        else:
+            sleep(2)
+            if not reachable_port:
+                print('[No Open Ports] Unable to connect to destination address...')
+                while not reachable_port:
+                    sleep(1)
+    if not secret_key:
+        for _ in range(4):
+            secret_key += chr(randrange(97, 123)).encode()
+    print(f"Connected")
+    print(f"Enter this Secret Key on Receiving Side: {secret_key.decode()}")
+    filename_iterator()
+
+
+
+def filename_iterator():
+    global FILES_CURRENTLY_SENDING, FILES_QUEUED_UP
     for full_path, folders, files in walk(getcwd()):
         for filename in files:
-            while True:
-                while FILES_CURRENTLY_SENDING >= MAX_SIMULTANEOUS_FILES:
-                    sleep(1)
-                connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                while True:
-                    try:
-                        connection.connect((new_server_ip, SERVER_MIGRATION_PORT))
-                        break
-                    except:
-                        pass
-                __send_to_connection(connection, secret_key)
-                if b'0' == __receive_from_connection(connection):
-                    FILES_CURRENTLY_SENDING += 1
-                    Thread(target=__send_individual_file, args=(connection, full_path, filename,)).start()
-                    break
+            while FILES_CURRENTLY_SENDING + FILES_QUEUED_UP >= MAX_SIMULTANEOUS_FILES:
+                sleep(0.5)
+            FILES_QUEUED_UP += 1
+            Thread(target=authenticate_with_receiver, args=(full_path, filename)).start()
+
+
+
+def authenticate_with_receiver(full_path, filename):
+    global secret_key, receiver_ip, reachable_port, FILES_CURRENTLY_SENDING, FILES_QUEUED_UP
+    connection = __force_connect_to_receiver(receiver_ip, reachable_port, 'tcp')
+    try:
+        if __receive_from_connection(connection) == secret_key:
+            __send_to_connection(connection, b'0')
+
+            Thread(target=__send_individual_file, args=(connection, full_path, filename,)).start()
+        else:
+            authenticate_with_receiver(full_path, filename)
+    except:
+        FILES_QUEUED_UP -= 1
+        authenticate_with_receiver(full_path, filename)
 
 
 def __send_individual_file(connection, full_path, filename):
-    global FILES_CURRENTLY_SENDING, FILES_SENT, SIZE_SENT
+    global FILES_CURRENTLY_SENDING, FILES_SENT, SIZE_SENT, FILES_QUEUED_UP
     try:
-            relative_path = full_path.replace(getcwd() + '/', '').replace(getcwd() + '\\', '').replace(getcwd(), '')
-            __send_to_connection(connection, path.join(relative_path, filename).replace('\\', '/').encode())
-            __send_to_connection(connection, str(stat(path.join(relative_path, filename).replace('\\', '/').encode()).st_size).encode())
-            with open(path.join(relative_path, filename), 'rb') as file:
-                while True:
-                    partial_file_data = file.read(BUFFER_SIZE)
-                    if partial_file_data:
-                        SIZE_SENT += len(partial_file_data)
-                        __send_to_connection(connection, partial_file_data)
-                    else:
-                        __send_to_connection(connection, b'{-0}')
-                        break
-            if __receive_from_connection(connection) == b'{-0}':
-                FILES_SENT += 1
-                FILES_CURRENTLY_SENDING -= 1
-                connection.close()
+        FILES_QUEUED_UP -= 1
+        FILES_CURRENTLY_SENDING += 1
+        relative_path = full_path.replace(getcwd() + '/', '').replace(getcwd() + '\\', '').replace(getcwd(), '')
+        __send_to_connection(connection, path.join(relative_path, filename).replace('\\', '/').encode())
+        with open(path.join(relative_path, filename), 'rb') as file:
+            while True:
+                partial_file_data = file.read(BUFFER_SIZE)
+                if partial_file_data:
+                    __send_file_data(connection, partial_file_data)
+                else:
+                    __send_file_data(connection, False)
+                    break
+        if __receive_from_connection(connection) == b'{-0}':
+            FILES_SENT += 1
+            FILES_CURRENTLY_SENDING -= 1
+            connection.close()
     except:
-        __send_individual_file(connection, full_path, filename)
+        authenticate_with_receiver(full_path, filename)
+
+
+
+def __send_file_data(connection, partial_file_data):
+    global SIZE_SENT
+    if partial_file_data:
+        data_length = len(partial_file_data)
+        SIZE_SENT += data_length
+        connection.send(f"{'0'*(15-len(str(data_length)))}{data_length}".encode())
+        connection.send(partial_file_data)
+        if connection.recv(1) == b'-':
+            return
+    else:
+        connection.send(f"{'0' * 15}".encode())
+        if connection.recv(1) == b'-':
+            return
+
 
 
 def __send_to_connection(connection, data_bytes: bytes):
+    global SIZE_SENT
     data_byte_length = len(data_bytes)
     connection.send(str(data_byte_length).encode())
     if connection.recv(1) == b'-':
         connection.send(data_bytes)
     if connection.recv(1) == b'-':
-        return
+        SIZE_SENT += data_byte_length
 
 
 def __receive_from_connection(connection):
+    global SIZE_RECEIVED
     length = int(connection.recv(BUFFER_SIZE))
     connection.send(b'-')
     data_bytes = b''
     while len(data_bytes) != length:
         data_bytes += connection.recv(BUFFER_SIZE)
     connection.send(b'-')
+    SIZE_RECEIVED += length
     return data_bytes
 
 
-def sending_complete():
-    global FINISHED
-    while FILES_CURRENTLY_SENDING == 0:
-        sleep(1)
-    s_time = time()
-    Thread(target=update_screen).start()
-    while FILES_CURRENTLY_SENDING != 0:
-        sleep(1)
-    f_time = time()
-    FINISHED = True
-    print('Sending Complete', flush=True)
-    print(f"Time Taken: {int(f_time-s_time)}seconds with Buffer_size: {BUFFER_SIZE}", flush=True)
-    print(f"Total Sent: {FILES_SENT} Files ({size_calculator(SIZE_SENT)})", flush=True)
-    input()
+
 
 def update_screen():
     global FILES_CURRENTLY_SENDING, FINISHED
+    while FILES_CURRENTLY_SENDING == 0:
+        sleep(0.1)
+    s_time = time()
     while not FINISHED:
-        if system() == 'Windows':
-            call('cls', shell=True)
+        if FILES_CURRENTLY_SENDING != 0:
+            sleep(0.5)
         else:
-            call('clear', shell=True)
-        print(f"Currently Sending: {FILES_CURRENTLY_SENDING} Files", flush=True)
-        print(f"Total Sent: {FILES_SENT} Files ({size_calculator(SIZE_SENT)})", flush=True)
-        sleep(1/2)
+            FINISHED = True
+        if not FINISHED:
+            if system() == 'Windows':
+                call('cls', shell=True)
+            else:
+                call('clear', shell=True)
+            print('-' * 15, flush=True)
+            print('SENDING REPORT', flush=True)
+            print('-' * 15, flush=True)
+            print(f"Currently: {FILES_CURRENTLY_SENDING} Files", flush=True)
+            print(f"Completed: {FILES_SENT} Files", flush=True)
+            print(f"Sent Data: {size_calculator(SIZE_SENT)}", flush=True)
+            print(f"Received Data: {size_calculator(SIZE_RECEIVED)}", flush=True)
+            print(f"Time Elapsed: {int(time()-s_time)} seconds", flush=True)
+    print('-' * 9, flush=True)
+    print('FINISHED')
+    print('-' * 9, flush=True)
+    input()
 
-Thread(target=__initial_authenticate, args=(input("Enter IP Address of Receiver> "),)).start()
-Thread(target=sending_complete).start()
+
+
+Thread(target=address_translator, args=(input("Enter Address of Receiver> "),)).start()
+Thread(target=update_screen).start()
 
